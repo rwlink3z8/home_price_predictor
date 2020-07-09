5d827aaafdab155928072b3976e618fe0a25dbba
Robert Link <rwlink3z8@gmail.com>
2020-07-09 14:03:04 -0500

    Add files via upload

--- app/templates/app.js
+++ app/templates/app.js
@@ -28,7 +28,7 @@ function onClickedEstimatePrice() {
   var yr_built = document.getElementById("uiYR")
   var estPrice = document.getElementById("uiEstimatedPrice");
   
-  var url = "http://127.0.0.1:5000/predict_home_price";
+  var url = "/api/predict_home_price";
   
   $.post(url, {
     total_sqft: parseFloat(sqft.value),
@@ -46,7 +46,7 @@ function onClickedEstimatePrice() {
   
 function onPageLoad() {
   console.log( "document loaded" );
-  var url = "http://127.0.0.1:5000/get_location_names"; 
+  var url = "/api/get_location_names";
   $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
