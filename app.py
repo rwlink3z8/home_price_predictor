dd688f2eea04acafe3ba3e541cae9908cda19b4b
Robert Link <rwlink3z8@gmail.com>
2020-07-09 14:01:56 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+from flask import render_template, flash, redirect
+import json
+import util
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('app.html')
+
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    lot_size = float(request.form['lot_size'])
+    bathrooms = int(request.form['bathrooms'])
+    bedrooms = int(request.form['bedrooms'])
+    yr_built = int(request.form['yr_built'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(total_sqft, lot_size, bedrooms, bathrooms, yr_built, location)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
